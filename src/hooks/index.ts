2191c57771f4da04621f2d679fe004d1b04a11be
tanpmqe180124 <155402912+tanpmqe180124@users.noreply.github.com>
2025-10-03 10:43:19 +0700

    Delete src directory

--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-// src/hooks/useInViewAnimation.ts
-// (Đã có file này, dùng lại cho PlayerType)
-export * from './useInViewAnimation';
--- src/utils/httpStatus.ts
+++
@@ -1,5 +0,0 @@
-const isUnauthorizedError = (status: any) => {
-  return status === 401 || status === 403;
-};
-
-export default isUnauthorizedError;
